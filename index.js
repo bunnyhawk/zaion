7d2e8b8da85965f1f78326beae1f1eed030435cc
rhigdon@audioeye.com <rhigdon@audioeye.com>
2021-11-05 17:15:27 -0700

    Adding some more click logic

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 
 
 (function () {
+  const wrapper = document.querySelector('.wrapper');
   const sections = document.querySelectorAll('section');
   const navLinks = document.querySelectorAll('nav a');
 
   const resetSections = () => sections.forEach((section) => {
+    section.addEventListener('click', event => { event.stopPropagation() }, false);
     section.classList.add('hidden');
   });
 
   resetSections();
 
-
+  wrapper.addEventListener("click", function () {
+    resetSections();
+  }, false);
 
   navLinks.forEach((link) => {
     link.addEventListener('click', event => {
+      event.stopPropagation();
       const sectionContainer = document.querySelector(`.${link.dataset.section}`);
       resetSections();
       sectionContainer.classList.toggle('hidden');
-    });
+    }, false);
   });
 })();
